4bb9a16d25c4b3fc6cbf8a9c07263f4af7fc830d
Nauj <juan.martin.m@gmail.com>
2019-10-14 23:37:17 +0200

    desplegable archivos

--- js/inlineform.js
+++ js/inlineform.js
@@ -159,10 +159,10 @@ var inlineform2 = ( function ($,undefined) {
 		$botoncomentarios.addClass('boton miniboton miniboton-inlineform boton-comentarios fa fa-commenting-o').css({'font-size':'10px',padding:'2px 0 0 3px'})
 		$wrapper.append ( $botoneliminar )
 		$wrapper.css({opacity:1}).append (
-			$('<div style="width:16px;height:16px;overflow:hidden;border:1px solid #ddd;z-index:;background:white;position:relative;margin-top:0px;"><div class="boton miniboton miniboton-inlineform" style="padding:0" title=""><div style="margin:-1px 0 0 3px">+</div></div></div>' ).append ( $botonavisos ).append ( $botoncomentarios ).append ( $botonfiles ).append ( $botonhistorico )
+			$('<div style="width:16px;height:16px;overflow:hidden;border:1px solid #ddd;z-index:;background:white;position:relative;margin-top:0px;"><div class="boton miniboton miniboton-inlineform" style="padding:2px 0" title=""><div style="margin:-1px 0 0 3px">+</div></div></div>' ).append ( $botonavisos ).append ( $botoncomentarios ).append ( $botonfiles ).append ( $botonhistorico )
 			.on ( 'mouseover' , function () {
 			var $this = $(this)
-			$this.width('auto')
+			$this.width('140')
 			console.log('aa')
 		}).on ( 'mouseout' , function () {
 			$(this).width(16)
